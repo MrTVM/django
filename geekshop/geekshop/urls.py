1721407ebdcd14db4b87ba84705705a14619536c
mrTVM <tv1adimir2011@gmail.com>
2019-07-11 23:32:49 +0300

    Настройка Медиа

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp import views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', mainapp.main),
@@ -23,3 +25,8 @@ urlpatterns = [
     path('contact/', mainapp.contacts, name='contact'),
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
