5a16bb097440e135c60649c6e7f9a9a440dc8c57
mrTVM <tv1adimir2011@gmail.com>
2019-07-19 21:34:24 +0300

    Сделал динамическую генерацию меню категорий и подсветку выбранной категории.

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, ProductCategory
 
 
@@ -6,10 +6,15 @@ def main(request):
     return render(request, 'mainapp/index.html')
 
 
-def products(request):
+def products(request, pk=None):
     title = 'Interior product'
     categories = ProductCategory.objects.all()
     products = Product.objects.all()
+
+    if pk:
+        category = get_object_or_404(ProductCategory, pk=pk)
+        products = products.filter(category=category)
+
     content = {
         'title': title,
         'categories': categories,
