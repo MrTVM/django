66f874da907e184d6054cd7b0c1656d72b5b4d56
mrTVM <tv1adimir2011@gmail.com>
2019-07-21 19:34:50 +0300

    Добавил вывод корзины. Защитил доступ к корзине декоратором @login_required.

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import HttpResponseRedirect, get_object_or_404
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404, HttpResponse
 from .models import BasketSlot
 from mainapp.models import Product
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
+def basket(request):
+    basket = []
+    if request.user.is_authenticated:
+        basket = request.user.basket.all()
+
+    return render(request, 'basketapp/basket.html', {'basket_items': basket})
+
+
+@login_required
 def add(request, product_pk=None):
     product = get_object_or_404(Product, pk=product_pk)
     old_basket_slot = BasketSlot.objects.filter(user=request.user, product=product).first()
@@ -17,6 +28,7 @@ def add(request, product_pk=None):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def remove(request, product_pk=None):
     product = get_object_or_404(Product, pk=product_pk)
     basket_slot = BasketSlot.objects.filter(user=request.user, product=product).first()
@@ -30,3 +42,26 @@ def remove(request, product_pk=None):
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+
+@login_required
+def edit(request, pk):
+    if request.is_ajax():
+        basket_slot = get_object_or_404(BasketSlot, pk=pk)
+
+        quantity = int(request.GET.get('quantity'))
+        if quantity > 0:
+            basket_slot.quantity = quantity
+            basket_slot.save()
+        else:
+            basket_slot.delete()
+
+        # basket_items = BasketSlot.objects.filter(user=request.user).order_by('product__category')
+        #
+        # context = {
+        #     'basket_items': basket_items,
+        # }
+        #
+        # result = render_to_string('basketapp/includes/inc_basket_list.html', context)
+        #
+        # return JsonResponse({'result': result})
+        return HttpResponse('Ok')
