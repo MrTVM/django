27e9b153b9bf14aeda33977377295ff7f41fbb2f
mrTVM <tv1adimir2011@gmail.com>
2019-07-27 18:32:21 +0300

    Добавил работу с товарами в админке.

--- geekshop/adminapp/forms.py
+++ geekshop/adminapp/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserChangeForm
 from authapp.models import ShopUser
 from authapp.forms import ShopUserRegisterForm
 from django import forms
-from mainapp.models import ProductCategory
+from mainapp.models import ProductCategory, Product
 
 
 class ShopUserAdminEditForm(UserChangeForm):
@@ -32,6 +32,18 @@ class ShopUserAdminRegisterForm(ShopUserRegisterForm):
 class ProductCategoryAdminForm(forms.ModelForm):
     class Meta:
         model = ProductCategory
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+
+class ProductAdminForm(forms.ModelForm):
+    class Meta:
+        model = Product
         fields = '__all__'
 
     def __init__(self, *args, **kwargs):
